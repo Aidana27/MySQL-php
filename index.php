b6e1bb32c2580b3f0e1df21d1d82d3c9928aa074
Aidana27 <75001581+Aidana27@users.noreply.github.com>
2021-04-20 23:11:22 +0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,25 +1,19 @@
+
 <table>
 	<tr>
 		<th>id</th>
 		<th>name</th>
 		<th>age</th>
 		<th>salary</th>
-		<th>delete</th>
 	</tr>
 	<?php
-		$link = mysqli_connect(
+		$query = "SELECT * FROM workers";
+        $link = mysqli_connect(
             'localhost',  
             'root',      
             'root',   
             'work'); 
-            
-		if (isset($_GET['del'])) {
-			$del = $_GET['del'];
-			$query = "DELETE FROM workers WHERE id=$del";
-			mysqli_query($link, $query) or die(mysqli_error($link));
-		}
-	
-		$query = "SELECT * FROM workers";
+
 		$result = mysqli_query($link, $query) or die(mysqli_error($link));
 		for ($data = []; $row = mysqli_fetch_assoc($result); $data[] = $row);
 		
@@ -31,8 +25,6 @@
 			$result .= '<td>' . $elem['name'] . '</td>';
 			$result .= '<td>' . $elem['age'] . '</td>';
 			$result .= '<td>' . $elem['salary'] . '</td>';
-			$result .= '<td>' . $elem['salary'] . '</td>';
-			$result .= '<td><a href="?del=' . $elem['id'] . '">удалить</a></td>';
 			
 			$result .= '</tr>';
 		}
@@ -40,6 +32,9 @@
 		echo $result;
 	?>
 </table>
+
+
+
 
 
 
